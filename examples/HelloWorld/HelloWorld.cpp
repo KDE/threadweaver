8eace8fa4adf5fafdea35321239f0d175ef40394
Mirko Boehm (Endocode) <mirko@endocode.com>
2014-08-11 12:11:53 +0200

    Add first examples from the ThreadWeaverDemos repository.

---
+++ examples/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿//@@snippet_begin(sample-helloworld)
+#include <QtCore>
+#include <ThreadWeaver/ThreadWeaver>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+
+    using namespace ThreadWeaver;
+    stream() << make_job( []() { qDebug() << "Hello World!"; } );
+}
+//@@snippet_end
