e98efb97f5b7259e7510d67ac69acce85e71cf1c
Nicolas Fella <nicolas.fella@gmx.de>
2021-02-01 17:14:54 +0100

    Fix map interators when building against Qt6
    In Qt6 we need to use QMutableMultiMapInterator for interating over a
    QMultiMap

--- src/dependencypolicy.cpp
+++ src/dependencypolicy.cpp
@@ -83,7 +83,11 @@ bool DependencyPolicy::removeDependency(JobPointer jobA, JobPointer jobB)
     QMutexLocker l(d->mutex());
 
     // there may be only one (!) occurrence of [this, dep]:
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+    QMutableMultiMapIterator<JobPointer, JobPointer> it(d->dependencies());
+#else
     QMutableMapIterator<JobPointer, JobPointer> it(d->dependencies());
+#endif
     while (it.hasNext()) {
         it.next();
         if (it.key() == jobA && it.value() == jobB) {
@@ -107,7 +111,11 @@ void DependencyPolicy::resolveDependencies(JobPointer job)
 {
     if (job->success()) {
         QMutexLocker l(d->mutex());
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+        QMutableMultiMapIterator<JobPointer, JobPointer> it(d->dependencies());
+#else
         QMutableMapIterator<JobPointer, JobPointer> it(d->dependencies());
+#endif
         // there has to be a better way to do this: (?)
         while (it.hasNext()) { // we remove all entries where jobs depend on *this* :
             it.next();
